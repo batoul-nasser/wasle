5f2c46e946d6907ab4940cb4a22fe37cc7c49b35
hayamabdallatif664 <hayamabdallatif664@gmail.com>
2026-04-22 02:16:23 +0300

    Fix OTP resend type

--- lib/features/auth/data/auth_service.dart
+++ lib/features/auth/data/auth_service.dart
@@ -141,7 +141,7 @@ class AuthService {
         .from('merchant_users')
         .select()
         .eq('profile_id', profileId)
-        .maybeSingle();
+        .maybeSingle();y
 
     return result;
   }
